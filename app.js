75636048f155d4badd9f4040dcd7a43962788d36
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-10-29 18:01:47 +0100

    Создал проект

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const cors = require('cors');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/moviesdb');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(errors());
+
+app.listen(PORT);
