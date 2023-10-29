66df889457ec0b8d3bd10ea65195a9d722c616d2
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-10-29 18:02:33 +0100

    Исправил название базы данных

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/moviesdb');
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
 app.use(cors());
 app.use(express.json());
