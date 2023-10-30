2a93808788c274063c51bd3598353837865ebffa
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-10-30 21:05:41 +0100

    Исправил нейминг

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,9 @@
-const cardsRouter = require('express').Router();
+const moviesRouter = require('express').Router();
 const { getMovies, postMovies, deleteMovies } = require('../controllers/movies');
 const { moviesRouteValidation, moviesIdRouteValidation } = require('../validators/movies');
 
-cardsRouter.get('', getMovies);
-cardsRouter.post('', moviesRouteValidation, postMovies);
-cardsRouter.delete('/:id', moviesIdRouteValidation, deleteMovies);
+moviesRouter.get('', getMovies);
+moviesRouter.post('', moviesRouteValidation, postMovies);
+moviesRouter.delete('/:id', moviesIdRouteValidation, deleteMovies);
 
-module.exports = cardsRouter;
+module.exports = moviesRouter;
